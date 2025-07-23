46763e9ada9bee44c0a2f0fdc45d8d9d8c8103d2
rajkumar chourasiya <rajchourasiya2862002@gmail.com>
2025-07-24 00:28:41 +0530

    Initial commit of server folder

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // CORS Setup
 app.use(cors({
-  origin: 'https://tech-rraj-client-repo-xwx8.vercel.app ',
+  origin: 'https://tech-rraj-client-repo-xwx8.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
