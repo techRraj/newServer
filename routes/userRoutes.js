687d231550caf0296d062be183f81cb05578c1b9
rajkumar chourasiya <rajchourasiya2862002@gmail.com>
2025-07-24 16:33:43 +0530

    Initial commit of server folder

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,16 @@
-// routes/userRoutes.js
 import express from 'express';
 import { registerUser, loginUser, userCredits, paymentRazorpay, verifyRazorpay } from '../controllers/userController.js';
 import authUser from '../middlewares/auth.js';
 
 const router = express.Router();
 
-// Utility to handle async errors
 function asyncHandler(fn) {
   return (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
 }
 
-// Public Routes
 router.post('/register', asyncHandler(registerUser));
 router.post('/login', asyncHandler(loginUser));
 
-// Protected Routes
 router.get('/credits', authUser, asyncHandler(userCredits));
 router.post('/pay-razor', authUser, asyncHandler(paymentRazorpay));
 router.post('/verify-razor', asyncHandler(verifyRazorpay));
--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.use(express.json());
 app.use(cors({
   origin: 'https://tech-rraj-client-repo-xwx8.vercel.app',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+
 }));
 
 // Routes
